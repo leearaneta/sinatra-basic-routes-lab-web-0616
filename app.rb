cea4b81e182c7716ee6c986160a9e2a0f6fff29c
leearaneta <lee.araneta@gmail.com>
2016-06-25 20:45:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		'Hello, World!'
+	end
+
+	get '/name' do
+		'My name is Lee'
+	end
+
+	get '/hometown' do
+		'My hometown is Somerset'
+	end
+
+	get '/favorite-song' do
+		'My favorite song is ur mom lol'
+	end
 end
